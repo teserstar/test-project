db7c7b5e93f09e8a173bef82cbea544c38d8d16b
Alex <starovaleksej@yandex.ru>
2017-10-02 02:26:47 +0400

    Move from unittest to py.test. Implementing driver fixture.
    Step 2

--- test_php4dvd.py
+++ test_php4dvd.py
@@ -1,27 +1,13 @@
 from selenium import webdriver
 from selenium.common.exceptions import *
-import unittest
+from selenium_fixture import driver
 
-class Untitled(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "http://localhost/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
-    def test_login(self):
-        driver = self.driver
-        driver.get(self.base_url + "/php4dvd/")
-        driver.find_element_by_id("username").clear()
-        driver.find_element_by_id("username").send_keys("admin")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("admin")
-        driver.find_element_by_name("submit").click()
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_login(driver):
+    driver.get("http://localhost/php4dvd/")
+    driver.find_element_by_id("username").clear()
+    driver.find_element_by_id("username").send_keys("admin")
+    driver.find_element_by_name("password").clear()
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("submit").click()
+    driver.find_element_by_link_text("Log out").click()
+    driver.switch_to_alert().accept()
